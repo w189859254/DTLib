702da7c44d8e974dee6846331b1a884c5f3c7393
wh <189859254@qq.com>
2018-04-30 16:29:15 +0800

    补提交90d9d0f

--- main.cpp
+++ main.cpp
@@ -1,36 +1,30 @@
 #include <iostream>
-#include "SmartPointer.h"
+#include "Exception.h"
 
 using namespace std;
 using namespace DTLib;
 
-class Test
-{
-public:
-    Test()
-    {
-        cout << "Test()" << endl;
-    }
 
-    ~Test()
-    {
-        cout << "Test()" << endl;
-    }
-
-};
 
 int main()
 {
-    SmartPointer<Test> sp = new Test();
-    SmartPointer<Test> nsp;
-
-
-    nsp = sp;
-
-    //nsp++;  //±àÒë´íÎó
-
-    cout << sp.isNull() << endl;
-    cout << nsp.isNull() << endl;
+    try
+    {
+        //throw Exception("test", __FILE__, __LINE__);
+        THROW_EXCEPTION(ArithmeticException, "test");
+    }
+    catch(const Exception& e)
+    {
+        cout << "catch (const Exception&e)" << endl;
+        cout << e.message() << endl;
+        cout << e.location() << endl;
+    }
+    catch(const ArithmeticException& e)
+    {
+        cout << " catch(const ArithmeticException& e)" << endl;
+        cout << e.message() << endl;
+        cout << e.location() << endl;
+    }
 
     return 0;
 }
