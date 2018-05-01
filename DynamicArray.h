da6c026a4e3a77fdbbe70da36a0ff8f8b9be9288
wh <189859254@qq.com>
2018-05-01 16:58:36 +0800

    优化动态数组

--- DynamicArray.h
+++ DynamicArray.h
@@ -11,64 +11,71 @@ class DynamicArray : public Array<T>
 {
 protected:
     int m_length;
-public:
 
-    DynamicArray(int length)
+    T* copy(T* array, int len, int newlen)
     {
-        this->m_array = new T[length];
+        T* ret = new T[newlen];
 
-        if(this->m_array != NULL)
+        if(ret != NULL)
         {
+            int size = (len < newlen) ? len : newlen;
+
+            for(int i=0; i<size; i++)
+            {
+                ret[i] = array[i];
+            }
+        }
+
+        return ret;
+    }
+
+    void update(T* array, int length)
+    {
+        if(array != NULL)
+        {
+            T* tmp = this->m_array;
+
+            this->m_array = array;
+            this->m_length = length;
+
+            delete[] tmp;
+        }
+        else
+        {
+            THROW_EXCEPTION(NoEnoughMemoryException, "No memory to update DynamicArray object ...");
+        }
+    }
+
+    void init(T* array, int length)
+    {
+        if(array != NULL)
+        {
+            this->m_array = array;
             this->m_length = length;
         }
         else
         {
             THROW_EXCEPTION(NoEnoughMemoryException, "No memory to create DynamicArray object ...");
         }
+    }
 
+public:
+
+    DynamicArray(int length)
+    {
+        init(new T[length], length);
     }
 
     DynamicArray(const DynamicArray<T>& obj)
     {
-        this->m_array = new T[obj.m_length];
-
-        if(this->m_array != NULL)
-        {
-            for(int i=0; i<obj.m_length; i++)
-            {
-                this->m_array[i] = obj.m_array[i];
-            }
-        }
-        else
-        {
-            THROW_EXCEPTION(NoEnoughMemoryException, "No memory to create DynamicArray object ...");
-        }
+        init(copy(obj.m_array, obj.m_length, obj.m_length), obj.m_length);
     }
 
     DynamicArray<T>& operator= (const DynamicArray<T>& obj)
     {
         if(this != &obj)
         {
-            T* array = new T[obj.m_length];
-
-            if(array != NULL)
-            {
-                for(int i=0; i<obj.m_length; i++)
-                {
-                    array[i] = obj.m_array[i];
-                }
-
-                T* tmp = this->m_array;
-
-                this->m_array = array;
-                this->m_length = obj.m_length;
-
-                delete[] tmp;
-            }
-        }
-        else
-        {
-            THROW_EXCEPTION(NoEnoughMemoryException, "No memory to copy object ...");
+            update(copy(obj.m_array, obj.m_length, obj.m_length), obj.m_length);
         }
 
         return *this;
@@ -83,28 +90,7 @@ public:
     {
         if(length != m_length)
         {
-            T* array = new T[length];
-
-            if(array != NULL)
-            {
-                int size = (length < m_length) ? length : m_length;
-
-                for(int i=0; i<size; i++)
-                {
-                    array[i] = this->m_array[i];
-                }
-
-                T* tmp = this->m_array;
-
-                this->m_array = array;
-                this->m_length = length;
-
-                delete[] tmp;
-            }
-            else
-            {
-                THROW_EXCEPTION(NoEnoughMemoryException, "No memory to resize object ...");
-            }
+            update(copy(this->m_array, m_length, length), length);
         }
     }
 
