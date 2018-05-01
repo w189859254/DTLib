5e1802d1116236aff34a52bacaddea05b72995c9
wh <189859254@qq.com>
2018-05-01 09:59:09 +0800

    实现动态链表

---
+++ DynamicList.h
@@ -0,0 +1,94 @@
+#ifndef DYNAMICLIST_H
+#define DYNAMICLIST_H
+
+
+#include "Seqlist.h"
+#include "Exception.h"
+
+namespace DTLib
+{
+
+template <typename T>
+class DynamicList : public SeqList<T>
+{
+protected:
+    int m_capacity; // Ë³Ðò´æ´¢¿Õ¼äµÄ´óÐ¡
+public:
+    DynamicList(int capacity)   // ÉêÇë¿Õ¼ä
+    {
+        this->m_array = new T[capacity];
+        if(this->m_array != NULL)
+        {
+            this->m_length = 0;
+            this->m_capacity = capacity;
+        }
+        else
+        {
+            THROW_EXCEPTION(NoEnoughMemoryException, "No memory to create DynamicList object ...");
+        }
+    }
+
+    int capacity() const
+    {
+        return m_capacity;
+    }
+
+    void resize(int capacity)
+    {
+        if(capacity != m_capacity)
+        {
+            T* array = new T[capacity];
+
+            if(array != NULL)
+            {
+                int length = (this->m_length < capacity? this->m_length : capacity);
+
+                for(int i=0; i<length; i++)
+                {
+                    array[i] = this->m_array[i];
+                }
+
+                T* tmp = this->m_array;
+
+                this->m_array = array;
+                this->m_length = length;
+                this->m_capacity = capacity;
+
+                delete[] tmp;
+            }
+        }
+        else
+        {
+            THROW_EXCEPTION(NoEnoughMemoryException, "No memory to resize DynamicList objec ...");
+        }
+    }
+
+    ~DynamicList()
+    {
+        delete[] this->m_array;
+    }
+
+};
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // DYNAMICLIST_H
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "StaticList.h"
+#include "DynamicList.h"
 #include "Exception.h"
 
 using namespace std;
@@ -7,7 +7,7 @@ using namespace DTLib;
 
 int main()
 {
-    StaticList<int, 5> l;
+    DynamicList<int> l(5);
 
     for(int i=0; i<l.capacity(); i++)
     {
@@ -18,6 +18,7 @@ int main()
     {
         cout << l[i] << endl;
     }
+    cout << endl;
 
     l[0] *= l[0];
     for(int i=0; i<l.length(); i++)
@@ -33,6 +34,23 @@ int main()
     {
         cout << e.message() << endl;
         cout << e.location() << endl;
+
+        l.resize(10);
+        l.insert(5, 50);
+    }
+    cout << endl;
+    for(int i=0; i<l.length(); i++)
+    {
+        cout << l[i] << endl;
+    }
+
+
+     l.resize(3);
+
+    cout << endl;
+    for(int i=0; i<l.length(); i++)
+    {
+        cout << l[i] << endl;
     }
 
     return 0;
