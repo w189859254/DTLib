f4ecc67edd65908c3550b53105b2dfdda035f84d
wh <189859254@qq.com>
2018-05-10 23:33:57 +0800

    添加双向链表

--- main.cpp
+++ main.cpp
@@ -1,38 +1,58 @@
 #include <iostream>
-#include "CircleList.h"
+#include "DualLinkList.h"
 
 using namespace std;
 using namespace DTLib;
 
-// n: 多少人玩游戏
-// s: 从多少号开始
-// m: 几号去死
-void josephus(int n, int s, int m)
-{
-    CircleList<int > cl;
-
-    for(int i=1; i<=n; i++)
-    {
-        cl.insert(i);
-    }
-
-    // 下标从 0 开始，步进为 2
-    cl.move(s-1, m-1);
-
-    while(cl.length() > 0)
-    {
-        cl.next();
-
-        cout << cl.current() << endl;
-
-        cl.remove(cl.find(cl.current()));
-    }
-}
-
-
 int main()
 {
-    josephus(41, 1, 3);
+    DualLinkList<int> dl;
+
+    for(int i=0; i<5; i++)
+    {
+        dl.insert(0, i);
+        dl.insert(0, 5);
+    }
+
+    for(int i=0; i<dl.length(); i++)  // O(n^2)
+    {
+        cout << dl.get(i) << endl;
+    }
+
+    for(dl.move(0); !dl.end(); dl.next())// O(n)
+    {
+        cout << dl.current() << endl;
+    }
+
+    cout << "begin 1" << endl;
+
+    for(dl.move(dl.length()-1); !dl.end(); dl.pre())// O(n)
+    {
+        cout << dl.current() << endl;
+    }
+
+    cout << "begin 2" << endl;
+
+    dl.move(dl.length()-1);
+
+    while(!dl.end())
+    {
+        if(dl.current() == 5)
+        {
+            cout << dl.current() << endl;
+
+            dl.remove(dl.find(dl.current()));
+        }
+        else
+        {
+            dl.pre();
+        }
+    }
+
+    for(dl.move(0); !dl.end(); dl.next())// O(n)
+    {
+        cout << dl.current() << endl;
+    }
 
     return 0;
 }
