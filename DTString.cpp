2ad0c634ada785859eef4615f0de95752edd866b
wh <189859254@qq.com>
2018-05-19 23:51:36 +0800

    实现 DTString 类

---
+++ DTString.cpp
@@ -0,0 +1,183 @@
+#include <cstring>
+#include <cstdlib>
+#include "DTString.h"
+#include "Exception.h"
+
+using namespace std;
+
+namespace DTLib
+{
+
+void String::init(const char *s)
+{
+    m_str = strdup(s);
+
+    if(m_str)
+    {
+        m_length = strlen(m_str);
+    }
+    else
+    {
+        THROW_EXCEPTION(NoEnoughMemoryException, "No memory to create String object ...");
+    }
+}
+
+String::String()
+{
+    init("");
+}
+
+String::String(const char *s)
+{
+    init(s ? s : "");
+}
+
+String::String(const String& s)
+{
+    init(s.m_str);
+}
+
+String::String(char c)
+{
+    char s[] = {c, '\0'};
+    init(s);
+}
+
+int String::length() const
+{
+    return m_length;
+}
+
+const char* String::str() const
+{
+    return m_str;
+}
+
+bool String::operator == (const String& s) const
+{
+    return (strcmp(m_str, s.m_str) == 0);
+}
+bool String::operator == (const char* s) const
+{
+    return (strcmp(m_str, s ? s : "") == 0);
+}
+
+bool String::operator != (const String& s) const
+{
+    return !(*this == s);
+}
+bool String::operator != (const char* s) const
+{
+    return !(*this == s);
+}
+
+bool String::operator > (const String& s) const
+{
+    return (strcmp(m_str, s.m_str) > 0);
+}
+bool String::operator > (const char* s) const
+{
+    return (strcmp(m_str, s ? s : "") > 0);
+}
+
+bool String::operator < (const String& s) const
+{
+    return (strcmp(m_str, s.m_str) < 0);
+}
+bool String::operator < (const char* s) const
+{
+    return (strcmp(m_str, s ? s : "") < 0);
+}
+
+bool String::operator >= (const String& s) const
+{
+    return (strcmp(m_str, s.m_str) >= 0);
+}
+bool String::operator >= (const char* s) const
+{
+    return (strcmp(m_str, s ? s : "") >= 0);
+}
+
+bool String::operator <= (const String& s) const
+{
+    return (strcmp(m_str, s.m_str) <= 0);
+}
+bool String::operator <= (const char* s) const
+{
+    return (strcmp(m_str, s ? s : "") <= 0);
+}
+
+String String::operator + (const String& s) const
+{
+    return (*this + s.m_str);
+}
+String String::operator + (const char* s) const
+{
+    String ret;
+    int len = m_length + strlen(s ? s : "");
+    char* str = reinterpret_cast<char*>(malloc(len + 1));
+
+    if(str)
+    {
+        strcpy(str, m_str);
+        strcat(str, s ? s : "");
+
+        free(ret.m_str);
+
+        ret.m_str = str;
+        ret.m_length = len;
+    }
+    else
+    {
+        THROW_EXCEPTION(NoEnoughMemoryException, "No memroty to add String values ...");
+    }
+
+    return ret;
+}
+
+String& String::operator += (const String& s)
+{
+    return (*this = *this + s);
+}
+String& String::operator += (const char* s)
+{
+    return (*this = *this + s);
+}
+
+String& String::operator = (const String& s)
+{
+    return (*this = s.m_str);
+}
+String& String::operator = (const char* s)
+{
+    if(m_str != s)
+    {
+        char* str = strdup(s ? s : "");
+
+        if(str)
+        {
+            free(m_str);
+
+            m_str = str;
+            m_length = strlen(m_str);
+        }
+        else
+        {
+            THROW_EXCEPTION(NoEnoughMemoryException, "No memory to assign new String value ...");
+        }
+    }
+}
+String& String::operator = (char c)
+{
+    char s[] = {c, '\0'};
+
+    return (*this = s);
+}
+
+String::~String()
+{
+    free(m_str);
+}
+
+}
+
--- main.cpp
+++ main.cpp
@@ -1,29 +1,56 @@
 #include <iostream>
-#include "LinkQueue.h"
-#include "StaticQueue.h"
+#include "DTString.h"
 #include "Exception.h"
 
 using namespace std;
 using namespace DTLib;
 
-class Test : public Object
+void test_1()
 {
-public:
-    Test()
-    {
-        cout << "Test()" << endl;
-    }
-    ~Test()
-    {
-        cout << "~Test()" << endl;
-    }
-};
+    cout << "test_1() begin ..." << endl;
 
+    String s;
+    s = 'D';
+
+    cout << s.str() << endl;
+    cout << s.length() << endl;
+    cout << (s == "D") << endl;
+    cout << (s > "CCC") << endl;
+
+    cout << endl;
+
+    s += " Delphi Tang ";
+    cout << s.str() << endl;
+    cout << s.length() << endl;
+    cout << (s == "D Delphi Tang ") << endl;
+
+    cout << "test_1() end ..." << endl;
+}
+
+void test_2()
+{
+    cout << "test_2() begin ..." << endl;
+
+    String a[] = {"E", "D", "C", "B", "A"};
+    String min = a[0];
+
+    for(int i = 0; i < 5; i++)
+    {
+        if(min > a[i])
+        {
+            min = a[i];
+        }
+    }
+    cout << "min = " << min.str() << endl;
+
+    cout << "test_2() end ..." << endl;
+}
 
 int main()
 {
-    LinkQueue<Test> lq;
-    StaticQueue<Test, 5> sq;
+    test_1();
+    cout << endl;
+    test_2();
 
     return 0;
 }
