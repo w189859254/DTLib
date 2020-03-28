9f565b80d8e0bfaad83f5a714270ce8e7215b8c1
wenhao <189859254@qq.com>
2020-03-28 17:20:13 +0800

    定义树与树节点

---
+++ TreeNode.h
@@ -0,0 +1,27 @@
+﻿#ifndef TREENODE_H
+#define TREENODE_H
+
+#include "Object.h"
+
+namespace DTLib
+{
+
+template <typename T>
+class TreeNode : public Object
+{
+public:
+    T value;
+    TreeNode<T> *parent = nullptr;
+
+    virtual ~TreeNode() = 0;
+};
+
+template <typename T>
+TreeNode<T>::~TreeNode()
+{
+
+}
+
+}
+
+#endif // TREENODE_H
